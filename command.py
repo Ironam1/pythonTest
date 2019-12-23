4f99e534dfaa807c32956e392a1be5bfdee6c1c8
Ironam1 <ironam1@icloud.com>
2019-12-23 12:07:46 -0500

    problem #3 finished. now to import file

--- command.py
+++ command.py
@@ -2,25 +2,31 @@ import sys
 
 s = sys.argv[1]
 print("sys.argv is", sys.argv[1])
-
+# print(s.split())
 
 def reverseS(s):
+    
     if sys.argv[2] == "-r":
         str = ""
         for i in s:
             str = i + str
-        return str
+
+    elif sys.argv[2] == "-w":
+        str = s.split()
+        str.reverse()
+        # print(word)
+    
+    return str
 
 
 print(reverseS(s))
 
-str = sys.argv[2]
-def reverseW(s):
-    if sys.argv[2] == "-w":
-        # str = ""
-        str.reverse()
-    return str
+
+# def reverseW(s):
+#     if sys.argv[2] == "-w":
+#         s.split(" ")
+#     return s
 
 
-print(reverseW(s))
+# print(reverseW(s))
 

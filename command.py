61e2f493f3e5785da15bd89628271ece7da16841
Ironam1 <ironam1@icloud.com>
2019-12-23 12:13:53 -0500

    added file import

--- command.py
+++ command.py
@@ -1,25 +1,26 @@
 import sys
-
+f = open("prompt.txt", "r")
+arg = f.read()
 s = sys.argv[1]
 print("sys.argv is", sys.argv[1])
 # print(s.split())
 
-def reverseS(s):
+def reverseS(arg):
     
     if sys.argv[2] == "-r":
         str = ""
-        for i in s:
+        for i in arg:
             str = i + str
 
     elif sys.argv[2] == "-w":
-        str = s.split()
+        str = arg.split()
         str.reverse()
         # print(word)
     
     return str
 
 
-print(reverseS(s))
+print(reverseS(arg))
 
 
 # def reverseW(s):
